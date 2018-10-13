77a0a2b139c47fb32feca8e798ed7690a0c9b4f1
William Foster <William_1foster@yahoo.co.uk>
2018-10-13 14:35:11 +0100

    Added requirements for CW1B: Question 1

---
+++ ASPCorePractice/Pages/NameInput.cshtml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ASPCorePractice.Pages
+{
+    public class NameInputModel : PageModel
+    {
+        public String name;
+        
+        public void OnGet()
+        {
+
+        }
+
+        public void OnPost()
+        {
+            this.name = Request.Form["name"];
+        }
+    }
+}
